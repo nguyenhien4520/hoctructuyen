b8be68fdc88e35f4dd7c354bbd476f96c145336c
nguyenhien4520 <thefaceexample@gmail.com>
2024-09-16 00:17:54 +0700

    commit

--- test.js
+++ test.js
@@ -1,17 +1,17 @@
-// import OpenAI from "openai";
-//require('dotenv').config();
+const OpenAI = require("openai");
+require('dotenv').config();
 
-// const openai = new OpenAI({
-//     apiKey: process.env.KEY
-// });
+const openai = new OpenAI({
+    apiKey: process.env.KEY
+});
 
-// async function main() {
-//   const completion = await openai.chat.completions.create({
-//     messages: [{ role: "system", content: "who is president of us" }],
-//     model: "gpt-4o-mini",
-//   });
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "who is president of us" }],
+    model: "gpt-4o-mini",
+  });
 
-//   console.log(completion.choices[0].message.content);
-// }
+  console.log(completion.choices[0].message.content);
+}
 
-// main();
+main();
